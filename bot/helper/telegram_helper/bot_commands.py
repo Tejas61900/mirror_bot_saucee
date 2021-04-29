3732e3cdb936dcb910088057954702bbfa2a8933
Tejas61900 <ubaletejas9@gmail.com>
2021-04-29 10:41:04 +0000

    sauce

---
+++ bot/helper/telegram_helper/bot_commands.py
@@ -0,0 +1,23 @@
+class _BotCommands:
+    def __init__(self):
+        self.StartCommand = 'start'
+        self.MirrorCommand = 'mirror3'
+        self.UnzipMirrorCommand = 'unzipmirror3'
+        self.TarMirrorCommand = 'tarmirror3'
+        self.CancelMirror = 'cancel3'
+        self.CancelAllCommand = 'cancelall3'
+        self.ListCommand = 'list3'
+        self.StatusCommand = 'status3'
+        self.AuthorizeCommand = 'authorize'
+        self.UnAuthorizeCommand = 'unauthorize'
+        self.PingCommand = 'ping3'
+        self.RestartCommand = 'restart3'
+        self.StatsCommand = 'stats3'
+        self.HelpCommand = 'help3'
+        self.LogCommand = 'log3'
+        self.CloneCommand = "clone3"
+        self.WatchCommand = 'watch3'
+        self.TarWatchCommand = 'tarwatch3'
+        self.deleteCommand = 'del3'
+
+BotCommands = _BotCommands()
